754b21c5e56a5bb2fa7500a8e75c05a0be1495fb
Yuri Kopshev <yura.kopshev@gmail.com>
2020-07-09 14:27:46 +0200

    add moneytransfer files

---
+++ Money Transfer/src/Transfer/MoneyTransfer.java
@@ -0,0 +1,10 @@
+package Transfer;
+
+public class MoneyTransfer {
+    public static void main(String[] args) {
+        int account = 2_000_000_000;
+        int transfer = 500_000_000;
+        int result=account+transfer;
+        System.out.println(result);
+    }
+}
